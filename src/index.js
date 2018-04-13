8eef849f7bfb4ced1577ee3cdf54a29d0ce4c507
Gaston Longhitano <gaston@juntacorp.com>
2018-04-13 12:33:33 -0400

    Extending the schema definition

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ const { GraphQLServer } = require('graphql-yoga')
 const typeDefs = `
 type Query {
 	info: String!
+	feed: [Link!]!
+}
+
+type Link {
+	id: ID!
+	description: String!
+	url: String!
 }
 `
 // 2
