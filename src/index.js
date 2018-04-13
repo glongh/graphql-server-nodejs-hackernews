3e003b55a089c42659c9f58fb049e6f984d394c7
Gaston Longhitano <gaston@juntacorp.com>
2018-04-13 14:32:49 -0400

    Moving the typeDef schema to it's own file

--- src/index.js
+++ src/index.js
@@ -6,20 +6,6 @@ let links = [{
   description: 'This is a description'
 }]
 
-// 1
-const typeDefs = `
-type Query {
-	info: String!
-	feed: [Link!]!
-}
-
-type Link {
-	id: ID!
-	description: String!
-	url: String!
-}
-`
-// 2
 const resolvers = {
 	Query: {
 		info: () => `This is the API of a Hackernews Clone`,
@@ -27,9 +13,8 @@ const resolvers = {
 	}
 }
 
-// 3
 const server = new GraphQLServer({
-	typeDefs, 
+	typeDefs: './src/schema.graphql', 
 	resolvers,
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
