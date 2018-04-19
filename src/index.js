af1d31b3aa2a7985ba7ccb83c07fb1cb6e44ebc5
Gaston Longhitano <gaston@juntacorp.com>
2018-04-19 15:56:59 -0400

    Creating the Prisma binding instance

--- src/index.js
+++ src/index.js
@@ -22,5 +22,14 @@ const resolvers = {
 const server = new GraphQLServer({
 	typeDefs: './src/schema.graphql', 
 	resolvers,
+	context: req => ({
+		...req,
+		db: new Prisma({
+			typeDefs: 'src/generated/prisma.graphql',
+	  		endpoint: 'http://localhost:4466/hackernews-node/dev',
+	  		secret: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InNlcnZpY2UiOiJoYWNrZXJuZXdzLW5vZGVAZGV2Iiwicm9sZXMiOlsiYWRtaW4iXX0sImlhdCI6MTUyMzkxMTI5NCwiZXhwIjoxNTI0NTE2MDk0fQ.y9N0JcDpmMQ8q4-P021-4VdwiiPV4pMBhxjm_WPfI7M',
+	  		debug: true,
+		})
+	}),
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
