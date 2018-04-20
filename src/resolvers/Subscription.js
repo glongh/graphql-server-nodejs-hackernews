9003cdac445b1dfcd09f48000213419dd52a4190
Gaston Longhitano <gaston@juntacorp.com>
2018-04-20 17:38:57 -0400

    Implementing new link subscription resolver

---
+++ src/resolvers/Subscription.js
@@ -0,0 +1,16 @@
+/**
+ * newLinkSubscribe Subscription resolver
+ */
+function newLinkSubscribe (parent, args, context, info) {
+	return context.db.subscription.link({ 
+		where: { mutation_in: ['CREATED'] } 
+	}, info)
+}
+
+const newLink = {
+	subscribe: newLinkSubscribe
+}
+
+module.exports = {
+	newLink
+}
